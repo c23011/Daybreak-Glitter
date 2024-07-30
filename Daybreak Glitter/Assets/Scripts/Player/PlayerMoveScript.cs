5605346af84df5ed78212299d742ff9538496ae7
c23011 <c23011@std.it-college.ac.jp>
2024-07-30 12:04:19 +0900

    GameObject.tagにwallを追加、プレイヤーがダッシュ中にwallに当たると反射するように

--- Daybreak Glitter/Assets/Scripts/Player/PlayerMoveScript.cs
+++ Daybreak Glitter/Assets/Scripts/Player/PlayerMoveScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
@@ -8,17 +9,66 @@ public class PlayerMoveScript : MonoBehaviour
     public float playerMoveSpeed;
     float playerMoveX;
     float playerMoveZ;
+    Vector3 lastVelocity;
+    bool ReflectSW;
+
+    public float dashTime;
+    public bool MoveSW;
+    public bool DashSW;
     void Start()
     {
-        
+        MoveSW = true;
     }
 
     void Update()
     {
-        playerMoveX = Input.GetAxisRaw("Horizontal") * playerMoveSpeed;
-        playerMoveZ = Input.GetAxisRaw("Vertical")   * playerMoveSpeed;
+        if (MoveSW == true)
+        {
+            playerMoveX = Input.GetAxisRaw("Horizontal") * playerMoveSpeed;
+            playerMoveZ = Input.GetAxisRaw("Vertical") * playerMoveSpeed;
 
-        PlayerRB.AddForce(playerMoveX, 0.0f, playerMoveZ);
-        PlayerRB.velocity = new Vector3(playerMoveX,-1.5f, playerMoveZ);
+            PlayerRB.velocity = new Vector3(playerMoveX, 0.0f, playerMoveZ);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                DashSW = true;
+                MoveSW = false;
+                Debug.Log("Dash!");
+            }
+        }
+
+        if (DashSW == true)
+        {
+            PlayerRB.velocity = new Vector3(playerMoveX*10, 0.0f, playerMoveZ*10);
+            Invoke("falseSW",dashTime);
+        }
+    }
+
+    void falseSW()
+    {
+        DashSW = false;
+        MoveSW = true;
+    }
+
+    void FixedUpdate()
+    {
+        lastVelocity = PlayerRB.velocity;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            ReflectSW = true;
+        }
+
+        if (ReflectSW == true)
+        {
+            Vector3 refrectVec = Vector3.Reflect(this.lastVelocity, collision.contacts[0].normal);//反射ベクトル計算
+            PlayerRB.velocity = refrectVec;
+            Debug.Log(refrectVec);
+        }
+
+        DashSW = false;
     }
 }
